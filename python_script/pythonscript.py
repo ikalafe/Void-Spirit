cea50e0337b32928151b80c439adb073b16cdecb
ikalafe <imkalafe@gmail.com>
2025-06-12 20:18:09 -0400

    Convert port input to integer and add basic TCP port scanning functionality

--- python_script/pythonscript.py
+++ python_script/pythonscript.py
@@ -2,4 +2,11 @@
 import socket
 
 ip = input("Enter the ip: ")
-port = input("Enter the port: ")
+port = int(input("Enter the port: "))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if s.connect_ex((ip, port)):
+    print(f"Port {port} is closed")
+else:
+    print(f"Port {port} is Open")
